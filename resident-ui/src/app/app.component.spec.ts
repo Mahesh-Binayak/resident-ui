3df35a62aadd840264add6c1be3fe5f1e603ba8d
Mahesh.Binayak <Mahesh.Binayak@technoforte.co.in>
2023-12-04 13:00:27 +0530

    [MOSIP-29316]kept just 1 test

--- resident-ui/src/app/app.component.spec.ts
+++ resident-ui/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,9 +10,6 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
-      ],
-      schemas: [
-        HeaderComponent
       ],
     }).compileComponents();
   }));
